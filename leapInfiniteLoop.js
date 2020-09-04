30d3aa038197307c9faa2513c5add30abd2e9d30
Jade O'Connor <jade.oconnor@uvm.edu>
2020-09-04 18:14:14 -0400

    we done with deliverable 1 baybee

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -5,14 +5,13 @@ var i;
 var z;
 var hand;
 var fingers;
+var tipPosition;
+var rawXMin = 100;
+var rawYMin = 100;
+var rawXMax = -100;
+var rawYMax = -100;
 
 function HandleFrame(frame) {
-    clear();
-    circle(x,y,50);
-    i = Math.floor(Math.random() * 3) - 1;
-    x += i;
-    z = Math.floor(Math.random() * 3) - 1;
-    y += z;
 
     if (frame.hands.length > 0) {
         hand = frame.hands[0];
@@ -24,20 +23,41 @@ function HandleHand(hand) {
     //console.log(frame.hands);
     //console.log(hand);
     fingers = hand.fingers;
-    console.log(fingers);
+    //console.log(fingers);
     for (var w = 0; w < fingers.length; w++) {
-        HandleFinger(fingers);
+        HandleFinger(fingers,w);
     }
 }
 
-function  HandleFinger(fingers) {
+function  HandleFinger(fingers,w) {
     if (fingers[w] == hand.indexFinger) {
-        console.log(fingers[w])
+        tipPosition = fingers[w].tipPosition;
+        console.log(tipPosition);
+        x = tipPosition[0];
+        y = -tipPosition[1];
+        z = tipPosition[2];
+        if (x < rawXMin) {
+            rawXMin = x;
+        }
+        if (x > rawXMax) {
+            rawXMax = x;
+        }
+        if (y < rawYMin) {
+            rawYMin = y;
+        }
+        if (y > rawYMax) {
+            rawYMax = y;
+        }
+
+        x = (0 + (window.innerWidth * ((x - rawXMin)/(rawXMax-rawXMin))));
+        y = (0 + (window.innerHeight * ((y - rawYMin)/(rawYMax-rawYMin))));
+        circle(x,y,50);
     }
 }
 
 Leap.loop(controllerOptions, function(frame)
     {
+        clear();
         HandleFrame(frame);
     }
 );
