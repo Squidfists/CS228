e7380296e23183ffa98873d9c6357583f8ad00f1
Jade O'Connor <jade.oconnor@uvm.edu>
2020-09-04 17:53:24 -0400

    refactor

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -5,29 +5,39 @@ var i;
 var z;
 var hand;
 var fingers;
+
+function HandleFrame(frame) {
+    clear();
+    circle(x,y,50);
+    i = Math.floor(Math.random() * 3) - 1;
+    x += i;
+    z = Math.floor(Math.random() * 3) - 1;
+    y += z;
+
+    if (frame.hands.length > 0) {
+        hand = frame.hands[0];
+        HandleHand(hand);
+    }
+}
+
+function HandleHand(hand) {
+    //console.log(frame.hands);
+    //console.log(hand);
+    fingers = hand.fingers;
+    console.log(fingers);
+    for (var w = 0; w < fingers.length; w++) {
+        HandleFinger(fingers);
+    }
+}
+
+function  HandleFinger(fingers) {
+    if (fingers[w] == hand.indexFinger) {
+        console.log(fingers[w])
+    }
+}
+
 Leap.loop(controllerOptions, function(frame)
     {
-        clear();
-        circle(x,y,50);
-        i = Math.floor(Math.random() * 3) - 1;
-        x += i;
-        z = Math.floor(Math.random() * 3) - 1;
-        y += z;
-
-        if (frame.hands.length > 0) {
-            //console.log(frame.hands);
-            hand = frame.hands[0];
-            //console.log(hand);
-            fingers = hand.fingers;
-            console.log(fingers);
-            for (var w = 0; w < fingers.length; w++) {
-                if (fingers[w] == hand.indexFinger) {
-                    console.log(fingers[w])
-                }
-
-            }
-        }
-
-
+        HandleFrame(frame);
     }
 );
