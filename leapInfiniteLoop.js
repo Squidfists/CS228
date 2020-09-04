d0c9c6dff9425ae844d09eb0a6ae7136427cd4b8
Jade O'Connor <jade.oconnor@uvm.edu>
2020-09-04 17:15:12 -0400

    jittery circle

--- leapInfiniteLoop.js
+++ leapInfiniteLoop.js
@@ -1,7 +1,14 @@
 var controllerOptions = {};
-var i = 0;
+var x = window.innerWidth/2;
+var y = window.innerHeight/2;
+var i;
 Leap.loop(controllerOptions, function(frame)
     {
-        console.log(i);
+        clear();
+        circle(x,y,50);
+        i = Math.floor(Math.random() * 3) - 1;
+        x += i
+        console.log(i)
+
     }
 );
