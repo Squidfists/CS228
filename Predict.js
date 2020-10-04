a602e9911c7472b97be450d46d79c226ea4d835c
Jade O'Connor <jade.oconnor@uvm.edu>
2020-10-04 17:18:06 -0400

    testing knn on garbage

--- Predict.js
+++ Predict.js
@@ -230,5 +230,4 @@ function draw(){
 
     Test();
     DrawCircles();
-
 }
--- PredictGestures.js
+++ PredictGestures.js
@@ -1,18 +1,41 @@
 const knnClassifier = ml5.KNNClassifier();
 
+var trainingCompleted = false;
+var predictedLabel;
+var testingSampleIndex = 0;
+var predictedClassLabels = nj.zeros([test.shape[3]]);
+
 function Train(){
     trainingCompleted = true;
     for (var l = 0; l < train0.shape[3]; l++){
         //console.log( train0.pick(null,null,null,l).toString() );
         var features = train0.pick(null,null,null,l);
         features = features.reshape(1,120);
-        //console.log(features.toString());
+
+        var features1 = train1.pick(null,null,null,l);
+        features1 = features1.reshape(1,120);
+
         knnClassifier.addExample(features.tolist(),0);
+        knnClassifier.addExample(features1.tolist(),1);
     }
+
 }
 
 function Test(){
+    for (var i = 0; i < test.shape[3]; i++){
+        var testfeatures = test.pick(null,null,null,i);
+        testfeatures = testfeatures.reshape(1,120);
+        predictedLabel = knnClassifier.classify(testfeatures.tolist(),GotResults);
+    }
+}
 
+function GotResults(err, result) {
+    predictedClassLabels.set(testingSampleIndex,parseInt(result.label));
+    console.log([testingSampleIndex,predictedClassLabels.get(testingSampleIndex)]);
+    testingSampleIndex++;
+    if (testingSampleIndex >= test.shape[3]) {
+        testingSampleIndex = 0;
+    }
 }
 
 
@@ -24,5 +47,4 @@ function draw(){
     }
 
     Test();
-
 }
