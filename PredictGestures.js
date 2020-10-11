81502bc1b58e1a4d444320d85cd8b11e870d2882
Jade O'Connor <jade.oconnor@uvm.edu>
2020-10-11 13:48:36 -0400

    deliverable 6

--- PredictGestures.js
+++ PredictGestures.js
@@ -67,8 +67,10 @@ function handleBone(bone,fingerIndex,boneIndex,InteractionBox) {
 
     framesOfData.set(fingerIndex,boneIndex,0, normalizedPrevJoint[0]);
     framesOfData.set(fingerIndex,boneIndex,1, normalizedPrevJoint[1]);
+    framesOfData.set(fingerIndex,boneIndex,2, normalizedPrevJoint[2]);
     framesOfData.set(fingerIndex,boneIndex,3, normalizedNextJoint[0]);
     framesOfData.set(fingerIndex,boneIndex,4, normalizedNextJoint[1]);
+    framesOfData.set(fingerIndex,boneIndex,5, normalizedNextJoint[2]);
 
     var canvasXPrev = window.innerWidth * normalizedPrevJoint[0];
     var canvasYPrev = window.innerHeight * (1 - normalizedPrevJoint[1]);
@@ -80,7 +82,7 @@ function handleBone(bone,fingerIndex,boneIndex,InteractionBox) {
 }
 function Train(){
     trainingCompleted = true;
-    for (var l = 0; l < train3.shape[3]; l++){
+    for (var l = 0; l < 2; l++){
         //console.log( train0.pick(null,null,null,l).toString() );
         var features = train3.pick(null,null,null,l);
         features = features.reshape(1,120);
@@ -88,8 +90,40 @@ function Train(){
         var features1 = train5.pick(null,null,null,l);
         features1 = features1.reshape(1,120);
 
+        var features2 = train7.pick(null,null,null,l);
+        features2 = features2.reshape(1,120);
+
+        var features3 = train1.pick(null,null,null,l);
+        features3 = features3.reshape(1,120);
+
+        var features4 = train0.pick(null,null,null,l);
+        features4 = features4.reshape(1,120);
+
+        var features5 = train2.pick(null,null,null,l);
+        features5 = features5.reshape(1,120);
+
+        var features6 = train4.pick(null,null,null,l);
+        features6 = features6.reshape(1,120);
+
+        var features7 = train6.pick(null,null,null,l);
+        features7 = features7.reshape(1,120);
+
+        var features8 = train8.pick(null,null,null,l);
+        features8 = features8.reshape(1,120);
+
+        var features9 = train9.pick(null,null,null,l);
+        features9 = features9.reshape(1,120);
+
+        knnClassifier.addExample(features4.tolist(),0);
+        knnClassifier.addExample(features3.tolist(),1);
+        knnClassifier.addExample(features5.tolist(),2);
         knnClassifier.addExample(features.tolist(),3);
+        knnClassifier.addExample(features6.tolist(),4);
         knnClassifier.addExample(features1.tolist(),5);
+        knnClassifier.addExample(features7.tolist(),6);
+        knnClassifier.addExample(features2.tolist(),7);
+        knnClassifier.addExample(features8.tolist(),8);
+        knnClassifier.addExample(features8.tolist(),9);
     }
 
 }
@@ -124,6 +158,21 @@ function centerData(){
             framesOfData.set(l,m,4, shiftedY);
         }
     }
+
+    zValues = framesOfData.slice([],[],[2,6,3]);
+    var currentzMean = zValues.mean();
+    var zShift = .5 - currentzMean;
+    for(var a = 0; a < framesOfData.shape[0]; a++){
+        for(var b = 0; b < framesOfData.shape[1]; b++){
+            currentZ = framesOfData.get(a,b,2);
+            shiftedZ = currentZ + zShift;
+            framesOfData.set(a,b,2, shiftedZ);
+
+            currentZ = framesOfData.get(a,b,5);
+            shiftedZ = currentZ + zShift;
+            framesOfData.set(a,b,5, shiftedZ);
+        }
+    }
 }
 
 function Test(){
@@ -136,7 +185,7 @@ function GotResults(err, result) {
     predictedClassLabels.set(0,parseInt(result.label));
     numPredictions++;
     var correct = 0;
-    if (predictedClassLabels.get(0) == 3){
+    if (predictedClassLabels.get(0) == 9){
         correct = 1;
     }
     else {
